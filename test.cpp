fce0bdc6b29cd74f02d15ee6d787fe2ca6fdd178
Zamir Noor <zamirnoor98@gmail.com>
2021-04-09 12:29:14 -0700

    Added lab1 files to lab2 repository

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include "c-echo.h"
+#include "gtest/gtest.h"
+
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, HelloWorld) {
+    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    EXPECT_EQ("hello world", echo(3,test_val));
+}
+TEST(EchoTest, SingleChar) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "J";
+    EXPECT_EQ("J", echo(2, test_val));
+}
+
+TEST(EchoTest, Space) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " ";
+    EXPECT_EQ(" ", echo(2, test_val));
+}
+
+TEST(EchoTest, SentenceString) {
+   char* test_val[6]; test_val[0] = "./c-echo"; test_val[1] = "Hello"; test_val[2] = "my"; test_val[3] = "name"; test_val[4] = "is"; test_val[5] = "Zamir.";
+   EXPECT_EQ("Hello my name is Zamir.", echo(6, test_val));
+}
+
+TEST(EchoTest, Numbers_SpecialChar) {
+   char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "1234567!@#$%^&*()";
+   EXPECT_EQ("1234567!@#$%^&*()", echo(2, test_val));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
