dda16fff75b5a0603a3a5b5129d9cdbeab69137c
anoor012 <anoor7122@gmail.com>
2021-04-09 15:40:11 -0700

    Revert "Updated main.cpp"
    This reverts commit 0e4176592cca3c478add7791dd41323e6e378d2b.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
-#include "c-echo.h"
+#include <iostream>
+#include <string>
 
-int main(int argv, char** argc) {
-    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
+int main()
+{
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
+
+    return 0;
 }
