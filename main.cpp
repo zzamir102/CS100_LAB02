482b08026b91431327d62b17012f219562030265
anoor012 <anoor7122@gmail.com>
2021-04-09 15:43:21 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <string>
 
-int main()
-{
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
+#include "c-echo.h"
+#include "c-count.h"
 
-    return 0;
+int main(int argv, char** argc) {
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
+
+
